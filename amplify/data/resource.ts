25170c97133db679a7f6b49c610426b8e8954f69
dareto-dream <dare.to.0ream@gmail.com>
2025-02-17 20:52:43 -1000

    ai lmao

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -7,6 +7,31 @@ specifies that any unauthenticated user can "create", "read", "update",
 and "delete" any "Todo" records.
 =========================================================================*/
 const schema = a.schema({
+    
+  chat: a.conversation({
+    aiModel: a.ai.model('Claude 3.5 Haiku'),
+    systemPrompt: 'You are a helpful assistant',
+  })
+  .authorization((allow) => allow.owner()),
+
+    
+  generateRecipe: a.generation({
+    aiModel: a.ai.model('Claude 3.5 Haiku'),
+    systemPrompt: 'You are a helpful assistant that generates recipes.',
+  })
+  .arguments({
+    description: a.string(),
+  })
+  .returns(
+    a.customType({
+      name: a.string(),
+      ingredients: a.string().array(),
+      instructions: a.string(),
+    })
+  )
+  .authorization((allow) => allow.authenticated()),
+
+  
   Todo: a
     .model({
       content: a.string(),
