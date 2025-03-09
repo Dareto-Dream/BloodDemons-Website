bd52e426a7de5464b901e3964bde12e2dd75afaa
dareto-dream <dare.to.0ream@gmail.com>
2025-03-09 01:07:07 -1000

    google stuf

---
+++ google/script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelector(".hero").style.opacity = "1";
+    document.querySelector(".hero").style.transform = "translateY(0)";
+});
